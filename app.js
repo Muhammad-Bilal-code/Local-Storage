2116014406c30a306111c577daddefebc9db7dfd
Muhammad-Bilal-code <96202090+Muhammad-Bilal-code@users.noreply.github.com>
2022-11-01 17:12:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,67 @@
 let inputField = document.getElementsByClassName("input-field")[0]
-console.log(inputField)
+// console.log(inputField)
 let inptFieldVal;
 
 let mainDiv = document.getElementsByClassName("main")[0]
-console.log(mainDiv)
+// console.log(mainDiv)
 let listItem;
 
+let gtitemArr
 let itemArr = [];
+let strItemArr;
+let parsItemArr;
+let getLocalItemArr;
+// console.log(getLocalItemArr)
 
-function handleAdd(){
-    inptFieldVal = inputField.value;
-    itemArr.push(inptFieldVal)
-    console.log(itemArr)
+strItemArr = JSON.stringify(itemArr)
+window.localStorage.setItem("itemArr",strItemArr)
+// console.log()
+
+
+console.log(itemArr)
+gtitemArr = window.localStorage.getItem("itemArr");
+console.log(gtitemArr)
+parsItemArr = JSON.parse(gtitemArr)
+console.log(parsItemArr)
+
+if(parsItemArr.length == 0){
     mainDiv.innerHTML = ""
+}
+else{
     for(i=0;i<itemArr.length;i++){
         listItem = document.createElement("input");
         listItem.setAttribute("class","list-item")
         mainDiv.appendChild(listItem)
-        console.log(itemArr[i])
+        // console.log(itemArr[i])
         listItem.value = itemArr[i]
     }
+}
+
+
+function handleAdd(){
+
+    console.log(parsItemArr)
+    inptFieldVal = inputField.value;
+    // console.log(inptFieldVal)
+    parsItemArr.push(inptFieldVal)
+    console.log(parsItemArr)
+    mainDiv.innerHTML = ""
+    for(i=0;i<parsItemArr.length;i++){
+        listItem = document.createElement("input");
+        listItem.setAttribute("class","list-item")
+        mainDiv.appendChild(listItem)
+        // console.log(itemArr[i])
+        listItem.value = parsItemArr[i]
+    }
+
+    strItemArr = JSON.stringify(parsItemArr)
+    window.localStorage.setItem("itemArr",strItemArr)
+
+}
+
+
+
+function handleClearLocal(){
+    window.localStorage.clear()
 }
 // console.log(itemArr)
