a42d2e9c65c632108fa92e4ca022187fc809cb79
Muhammad-Bilal-code <96202090+Muhammad-Bilal-code@users.noreply.github.com>
2022-11-03 17:41:04 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,58 +6,69 @@ let mainDiv = document.getElementsByClassName("main")[0]
 // console.log(mainDiv)
 let listItem;
 
-let gtitemArr
+let gtitemArr = [26]
 let itemArr = [];
 let strItemArr;
 let parsItemArr;
-let getLocalItemArr;
+let updatedArr ;
 // console.log(getLocalItemArr)
-
-strItemArr = JSON.stringify(itemArr)
-window.localStorage.setItem("itemArr",strItemArr)
-// console.log()
-
-
 console.log(itemArr)
-gtitemArr = window.localStorage.getItem("itemArr");
-console.log(gtitemArr)
-parsItemArr = JSON.parse(gtitemArr)
-console.log(parsItemArr)
 
-if(parsItemArr.length == 0){
-    mainDiv.innerHTML = ""
+gtitemArr = JSON.parse(window.localStorage.getItem("itemArr"))
+console.log(gtitemArr)
+// console.log(gtitemArr.length)
+
+if(gtitemArr){
+    updatedArr = [...gtitemArr]
+    console.log(updatedArr)
+    console.log(updatedArr.length)
 }
-else{
-    for(i=0;i<itemArr.length;i++){
-        listItem = document.createElement("input");
-        listItem.setAttribute("class","list-item")
-        mainDiv.appendChild(listItem)
-        // console.log(itemArr[i])
-        listItem.value = itemArr[i]
-    }
+// updatedArr = [...gtitemArr]
+console.log(updatedArr)
+console.log(updatedArr.length)
+
+
+if(updatedArr.length != 0){
+for(i=0;i<updatedArr.length;i++){
+    // console.log("test")
+    listItem = document.createElement("input");
+    listItem.setAttribute("class","list-item")
+    mainDiv.appendChild(listItem)
+    // console.log(itemArr[i])
+    listItem.value = updatedArr[i]
 }
+}
+
+
+// for(i=0;i<gtitemArr.length;i++){
+//     console.log(gtitemArr[i])
+// }
 
 
 function handleAdd(){
 
-    console.log(parsItemArr)
+    // console.log(parsItemArr)
     inptFieldVal = inputField.value;
     // console.log(inptFieldVal)
-    parsItemArr.push(inptFieldVal)
-    console.log(parsItemArr)
+    itemArr.push(inptFieldVal)
+    console.log(itemArr)
+    updatedArr = [...itemArr,...gtitemArr];
+    console.log(updatedArr)
     mainDiv.innerHTML = ""
-    for(i=0;i<parsItemArr.length;i++){
+    for(i=0;i<updatedArr.length;i++){
         listItem = document.createElement("input");
         listItem.setAttribute("class","list-item")
         mainDiv.appendChild(listItem)
         // console.log(itemArr[i])
-        listItem.value = parsItemArr[i]
+        listItem.value = updatedArr[i]
     }
-
-    strItemArr = JSON.stringify(parsItemArr)
+    
+    strItemArr = JSON.stringify(updatedArr)
     window.localStorage.setItem("itemArr",strItemArr)
 
+    inputField.value = ""
 }
+console.log(itemArr)
 
 
 
